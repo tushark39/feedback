23aacfc3730c4873ac91590ce5b4999689200428
tushark39 <tushark39@gmail.com>
2020-11-28 03:24:14 +0530

    Updates

---
+++ precache-manifest.3c4140ba355acca479e5977b9406aecb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657789c492902981e0d94b7b362ae825",
+    "url": "/feedback/index.html"
+  },
+  {
+    "revision": "0cb58412d647fa8b6710",
+    "url": "/feedback/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6cf24d77943a7298ba86",
+    "url": "/feedback/static/js/2.95eb3bc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/feedback/static/js/2.95eb3bc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb58412d647fa8b6710",
+    "url": "/feedback/static/js/main.96637e6a.chunk.js"
+  },
+  {
+    "revision": "4d40dd1ada65aa691441",
+    "url": "/feedback/static/js/runtime-main.09a19923.js"
+  }
+]);
